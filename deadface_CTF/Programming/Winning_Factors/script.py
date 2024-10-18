178e8edb17f1c54033a80260c4773a691fc85680
Killian Morin <killiancham@gmail.com>
2024-10-18 23:50:31 +0200

    deadface Programming

---
+++ deadface_CTF/Programming/Winning_Factors/script.py
@@ -0,0 +1,45 @@
+from pwn import *
+
+# Set architecture, os, and log level
+context.arch = "amd64"
+context.os = "linux"
+context.log_level = "debug"
+
+hostname = '147.182.245.126'
+port = 33001
+
+# Create a remote connection
+p = remote(hostname, port)
+
+# Read / ignore until the data that we want
+p.recvuntil("Calculate the factorial of")
+# Read the number, strip() to get rid of the spaces, pass from bytes to string and get rid of the trailing '.'
+number = p.recvline().strip().decode()[-1]
+print(f"number: {number}") # Decode the bytes to string
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+factorial = factorial(int(number))
+print(f"factorial: {factorial}")
+
+# Send the factorial back to the server
+p.send(f"{factorial}")
+p.send(b"\n")
+
+def print_lines(io):
+    info("Printing io received lines")
+    while True:
+        try:
+            line = io.recvline()
+            success(line.decode())
+        except EOFError:
+            break
+
+print_lines(p)
+
+# When you're done, close the connection
+p.close()
---
+++ deadface_CTF/Programming/Winning_Factors/writeup.md
@@ -0,0 +1,13 @@
+# Clue / Information
+As another test of your programming prowess, Turbo Tactical wants to see if you can write a script to solve mathematic equations being served by a remote server within 3 seconds.
+
+Submit the flag as flag{flag-text}
+
+147.182.245.126:33001
+
+# Resolution
+LOST WAY TOO MUCH ON CALCULATING FACTORS INSTEAD OF FACTORIALS FJDSKFJDSKLéAFFJDSAKLéDFJDSLF
+
+just run the script bro and if you don't have the environment do a `make` and then run the script ...
+
+the server outputs `Calculate the factorial of {int}.` and we have to send the factorial result of the int
